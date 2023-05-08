fc09d0cea25730904197efe16bb628ba570dcc20
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-05-08 17:46:53 +0200

    fix: revert to serverless function - incompatibility with @twurple/api using eval

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -14,6 +14,7 @@
  *
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next"
 
 /** Replace this with an object if you want to pass things to `createContextInner`. */
 type CreateContextOptions = Record<string, never>
@@ -40,7 +41,7 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = () => {
+export const createTRPCContext = (_opts: CreateNextContextOptions) => {
 	return createInnerTRPCContext({})
 }
 
